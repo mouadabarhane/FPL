e44c949503c66a66cc9646608119783cea2eac5c
mouadabarhane <76260053+mouadabarhane@users.noreply.github.com>
2022-11-13 12:15:50 +0100

    Add files via upload

---
+++ TpFreeFem/doc.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#flip").click(function() {
+        $("#panel").fadeToggle("slow");
+    });
+});
+$(document).ready(function() {
+    $("#flipe").click(function() {
+        $("#panele").fadeToggle("slow");
+    });
+});
+$(document).ready(function() {
+    $("#flipee").click(function() {
+        $("#panelee").fadeToggle("slow");
+    });
+});
+$(document).ready(function() {
+    $("#flipeeo").click(function() {
+        $("#paneleeo").fadeToggle("slow");
+    });
+});
+$(document).ready(function() {
+    $("#flipm").click(function() {
+        $("#panelm").fadeToggle("slow");
+    });
+});
+$(document).ready(function() {
+    $("#flipg").click(function() {
+        $("#panelg").fadeToggle("slow");
+    });
+});
+
+$(document).ready(function() {
+    $("#flips").click(function() {
+        $("#panels").fadeToggle("slow");
+    });
+});
